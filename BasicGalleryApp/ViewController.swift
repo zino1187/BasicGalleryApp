3c118f0a728803d11ee2aaba2c3abf4d2411a6d2
zino <minzino@hanmail.net>
2020-02-03 21:01:02 +0900

    last commit

--- BasicGalleryApp/ViewController.swift
+++ BasicGalleryApp/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgView: UIImageView!
+    var num:Int = 0
+    
+    @IBAction func showImg(_ sender: Any) {
+        num += 1
+        
+        imgView.image = UIImage(named: String(format: "h%d.png", num))
+        
+        if num%6==0{
+            num = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
